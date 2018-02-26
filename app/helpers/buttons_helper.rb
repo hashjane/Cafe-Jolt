b6f01e034b362280316e602207a26b732b7f5d4e
hashjane <hashjane@gmail.com>
2018-02-26 13:50:47 +1300

    added a view helper - that queries line_item for id where menu_item_id is given

---
+++ app/helpers/buttons_helper.rb
@@ -0,0 +1,9 @@
+module ButtonsHelper
+  def get_line_item_id(menu_item_id)
+
+    item = Array.new
+    item = @tray.line_items.where( menu_item_id: menu_item_id, tray_id: @tray.id)
+
+    item.ids
+  end
+end
